e31f522696aa043b92b630eb68fa2312cd893b2b
Matias Vargas <matiasvargas598@gmail.com>
2022-04-01 11:45:19 -0300

    Add files via upload

---
+++ Synapse SQL pool/Azure Synapase SQL Pool.sql
@@ -0,0 +1,38 @@
+-- CREATE master KEY
+
+CREATE DATABASE scoped credential PolyDemoCredential
+with
+identity = 'SecretIdentity',
+secret = '59UeFAPyJqHNXevcJOBP2Gj3MIyQP8nFjymyLsS5vVFqiQVOGEwOjilpw5ELt/XT0MYe7V6DpE6z+AStJ9rHfg==';
+
+CREATE EXTERNAL DATA SOURCE PolybaseDemoDataSource
+WITH
+(
+    TYPE = HADOOP,
+    LOCATION = 'wasbs://polybasedemocontainer@courserademostorage.blob.core.windows.net/',
+    CREDENTIAL = PolyDemoCredential
+);
+
+
+CREATE EXTERNAL FILE FORMAT polybasedemofileformat
+WITH
+(
+    FORMAT_TYPE = DELIMITEDTEXT,
+    FORMAT_OPTIONS (
+        FIELD_TERMINATOR = ',',
+        FIRST_ROW = 2)
+);
+
+CREATE EXTERNAL TABLE dbo.customer
+(
+    customerid int,
+    customername VARCHAR(1000),
+    city VARCHAR(1000)
+)
+WITH (
+    LOCATION = '/',
+    DATA_SOURCE = PolybaseDemoDataSource,
+    FILE_FORMAT = polybasedemofileformat
+)
+
+SELECT * FROM customer;
